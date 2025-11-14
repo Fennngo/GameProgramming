1653c4e948813c2dda9799f98478e9e320c4e8ae
Fennngo <1047934231@qq.com>
2025-11-14 23:49:38 +0000

    BigCarController

--- RemakeGame/Assets/Scripts/BigCarController.cs
+++ RemakeGame/Assets/Scripts/BigCarController.cs
@@ -16,32 +16,46 @@ public class BigCarController : MonoBehaviour
         public WheelCollider wheelCollider;
         public Axel axel;
     }
-    public List<Wheel> wheels;
+    
 
     public float maxAcceleration = 30.0f;
     public float brakeAcceleration = 50.0f;
 
+    public float turnSensitivity = 1.0f;
+    public float maxSteerAngle = 30.0f;
+
+    public Vector3 _centerOfMass;
+
+    public List<Wheel> wheels;
+
     float moveInput;
+    float steerInput;
 
     private Rigidbody carRb;
+      
     void Start()
     {
         carRb = GetComponent<Rigidbody>();
+        carRb.centerOfMass = _centerOfMass;
     }
 
     void Update()
     {
         GetInputs();
+        AnimateWheels();
     }
 
     void LateUpdate()
     {
         Move();
+        Steer();
+        Barke();
     }
 
     void GetInputs()
     {    
-            moveInput = Input.GetAxis("Vertical");
+        moveInput = Input.GetAxis("Vertical");
+        steerInput = Input.GetAxis("Horizontal");
     }
 
     void Move()
@@ -51,4 +65,47 @@ public class BigCarController : MonoBehaviour
             wheel.wheelCollider.motorTorque = moveInput * 600 * maxAcceleration * Time.deltaTime;
         }
     }
+    void Steer()
+    {
+        foreach (var wheel in wheels)
+        {
+            if (wheel.axel == Axel.Front)
+            {
+                var _steerAngle = steerInput * turnSensitivity * maxSteerAngle;
+                wheel.wheelCollider.steerAngle = Mathf.Lerp(wheel.wheelCollider.steerAngle, _steerAngle, 0.6f);
+            }
+        }
+    }
+
+    void Barke()
+    {
+        if (Input.GetKey(KeyCode.Space) || moveInput == 0)
+        {
+            foreach (var wheel in wheels)
+            {
+                wheel.wheelCollider.brakeTorque = 300 * brakeAcceleration * Time.deltaTime;
+
+            }
+        }
+
+        else
+        {
+            foreach (var wheel in wheels)
+            {
+                wheel.wheelCollider.brakeTorque = 0;
+            }
+        }
+    }
+    void AnimateWheels()
+    {
+        foreach (var wheel in wheels)
+        {
+            Quaternion rot;
+            Vector3 pos;
+            wheel.wheelCollider.GetWorldPose(out pos, out rot);
+            wheel.wheelModel.transform.position = pos;
+            wheel.wheelModel.transform.rotation = rot;
+        }
+    }
+
 }
