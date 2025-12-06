69d88f3af1a79c9e0063609defe9f55acb1680b5
Fennngo <1047934231@qq.com>
2025-12-06 01:19:43 +0000

    change script

--- RemakeGame/Assets/Scripts/BigCarController.cs
+++ RemakeGame/Assets/Scripts/BigCarController.cs
@@ -31,14 +31,9 @@ public class BigCarController : MonoBehaviour
     public List<Wheel> wheels;
 
     [Header("Speed Effects")]
-    public Transform cameraObj;
     public Material speedLineMat;
     public float effectStartSpeed = 30f;
     public float effectFullSpeed = 50f;
-    public float shakePower = 0.1f;
-
-    private float _currentShaderVal = 0f;
-    private Vector3 _originalCamPos;
 
     float moveInput;
     float steerInput;
@@ -50,11 +45,6 @@ public class BigCarController : MonoBehaviour
         carRb = GetComponent<Rigidbody>();
         carRb.centerOfMass = _centerOfMass;
 
-        if (cameraObj == null && Camera.main != null)
-            cameraObj = Camera.main.transform;
-
-        if (cameraObj != null)
-            _originalCamPos = cameraObj.localPosition;
     }
 
     void Update()
@@ -66,6 +56,25 @@ public class BigCarController : MonoBehaviour
         float speed = carRb.linearVelocity.magnitude;
 
         Debug.Log("speed: " + speed.ToString("F2") + " m/s");
+
+        if (speedLineMat != null)
+        {
+            float currentSpeed = carRb.linearVelocity.magnitude; 
+
+         
+            if (currentSpeed > effectStartSpeed)
+            {
+                speedLineMat.SetInt("_IsOpen", 1); 
+
+                float t = Mathf.InverseLerp(effectStartSpeed, effectFullSpeed, currentSpeed);
+                speedLineMat.SetFloat("_Speed", t);
+            }
+            else
+            {
+                speedLineMat.SetInt("_IsOpen", 0); 
+                speedLineMat.SetFloat("_Speed", 0);
+            }
+        }
 
     }
 
@@ -147,31 +156,6 @@ public class BigCarController : MonoBehaviour
             }
         }
     }
-    void HandleSpeedEffects(float currentSpeed)
-    {
-        if (speedLineMat == null) return;
-        float targetVal = 0f;
-        int isOpen = 0;
-
-        if (currentSpeed > effectStartSpeed)
-        {
-            isOpen = 1;
-            targetVal = Mathf.InverseLerp(effectStartSpeed, effectFullSpeed, currentSpeed);
-        }
-
-        _currentShaderVal = Mathf.Lerp(_currentShaderVal, targetVal, Time.deltaTime * 5f);
-        speedLineMat.SetInt("_IsOpen", isOpen);
-        speedLineMat.SetFloat("_Speed", _currentShaderVal);
-        if (cameraObj != null && currentSpeed > effectStartSpeed)
-        {
-            Vector3 shake = UnityEngine.Random.insideUnitSphere * shakePower;
-            if (currentSpeed > effectFullSpeed) shake *= 1.5f;
-            cameraObj.localPosition = _originalCamPos + shake;
-        }
-        else if (cameraObj != null)
-        {
-            cameraObj.localPosition = Vector3.Lerp(cameraObj.localPosition, _originalCamPos, Time.deltaTime * 5f);
-        }
-    }
+    
 
 }
