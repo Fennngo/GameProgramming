2d656512dcbf6b35c4f685794ad64802ce117227
Fennngo <1047934231@qq.com>
2025-12-08 02:29:46 +0000

    Scan system

---
+++ RemakeGame/Assets/Scripts/Scan.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SonarAbility : MonoBehaviour
+{
+    [Header("Setting")]
+    public float cooldownTime = 30f; 
+    public KeyCode triggerKey = KeyCode.E;
+    public float scanSpeed = 30f;    
+    public float maxRange = 150f;
+    public ParticleSystem sonarVFX;    
+    public AudioSource audioSource;    
+
+    [Header("Sound")]
+    public AudioClip scanSound;        
+    public AudioClip rechargeSound;   
+
+    private float lastScanTime = -999f;
+    private bool isReady = true;
+
+    void Update()
+    {
+        if (!isReady)
+        {
+            if (Time.time > lastScanTime + cooldownTime)
+            {
+                isReady = true;
+                if (audioSource && rechargeSound) audioSource.PlayOneShot(rechargeSound);
+            }
+        }
+
+        if (Input.GetKeyDown(triggerKey) && isReady)
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        isReady = false;
+        lastScanTime = Time.time;
+
+        if (audioSource && scanSound) audioSource.PlayOneShot(scanSound);
+        if (sonarVFX != null) sonarVFX.Play();
+
+        StartCoroutine(ScanWaveRoutine());
+
+        if (audioSource != null && scanSound != null)
+        {
+            Debug.Log("ÕýÔÚ²¥·ÅÉùÒô£º" + scanSound.name);
+            audioSource.PlayOneShot(scanSound, 5.0f);
+        }
+    }
+
+    IEnumerator ScanWaveRoutine()
+    {
+        float currentRadius = 0f;
+
+        HashSet<GameObject> scannedObjects = new HashSet<GameObject>();
+
+        while (currentRadius < maxRange)
+        {
+            currentRadius += scanSpeed * Time.deltaTime;
+            Collider[] hits = Physics.OverlapSphere(transform.position, currentRadius);
+            foreach (var hit in hits)
+            {
+                if (!scannedObjects.Contains(hit.gameObject))
+                {
+                    ScannableObject target = hit.GetComponent<ScannableObject>();
+                    if (target != null)
+                    {
+                        target.OnScanned();
+                        scannedObjects.Add(hit.gameObject);
+                    }
+                }
+            }
+            yield return null;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, maxRange);
+    }
+}
